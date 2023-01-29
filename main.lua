ea2349f3316ce1dbefb8dc7be5514d303e897712
Rubi Lotus <116446349+NotRubi@users.noreply.github.com>
2023-01-29 10:45:23 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,19 +3,5 @@ discord.gg/thedevs
 credits to Exunys (https://github.com/Exunys)
 ]]
 
-local Players = game:GetService("Players")
-local OldNameCall = nil
-
-OldNameCall = hookmetamethod(game, "__namecall", function(Self, ...)
-    local NameCallMethod = getnamecallmethod()
-
-    if tostring(string.lower(NameCallMethod)) == "kick" then
-          error("Bypassed Kick")
-        return nil
-    end
-    
-    return OldNameCall(Self, ...)
-end)
-
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Anti-Kick/main/Anti%20Kick.lua"))()
 loadstring(game:HttpGet("https://gitlab.com/S4nZz/sanz/-/raw/main/peesex"))()
