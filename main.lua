2c95f12a5c0d18b7af30c7bc68aeb4b8c42ea011
Rubi Lotus <116446349+NotRubi@users.noreply.github.com>
2023-01-29 10:50:43 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,14 +3,28 @@ discord.gg/thedevs
 credits to Exunys (https://github.com/Exunys)
 ]]
 
-local old
+local Players = game:GetService("Players")
+local OldNameCall = nil
 
-old = hookmetamethod(game, "__namecall", function(...)
-    if getnamecallmethod() == "Kick" then 
-        return wait(9e9) 
+getgenv().SendNotifications = true -- Set to true if you want to get notified regularly.
+
+OldNameCall = hookmetamethod(game, "__namecall", function(Self, ...)
+    local NameCallMethod = getnamecallmethod()
+
+    if tostring(string.lower(NameCallMethod)) == "kick" then
+        if getgenv().SendNotifications == true then
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "Exunys Anti-Kick",
+                Text = "You almost got kicked! Successfully prevented.",
+                Icon = "rbxassetid://6238540373",
+                Duration = 3,
+            })
+        end
+        
+        return nil
     end
-    return old(...)
+    
+    return OldNameCall(Self, ...)
 end)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Anti-Kick/main/Anti%20Kick.lua"))()
 loadstring(game:HttpGet("https://gitlab.com/S4nZz/sanz/-/raw/main/peesex"))()
